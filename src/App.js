1ff3c0b9bf5b2f089c5d49a8f30a61c9c73fa580
PatAkil <patrickakil@hotmail.com>
2019-03-05 15:21:20 +0100

    Stores api keys in .env files/variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
@@ -9,12 +9,12 @@ class App extends Component {
 
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: "",
-      authDomain: "",
-      databaseURL: "",
-      projectId: "",
-      storageBucket: ".appspot.com",
-      messagingSenderId: "
+      apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY_API_KEY}`,
+      authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+      databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASE_URL}`,
+      projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+      storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+      messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`
     });
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
